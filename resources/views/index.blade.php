e1b522c6a495ca4149c384966446f90ae3d8bf90
Enrique Rodriguez <enrique.rv0704@gmail.com>
2021-01-17 14:45:08 -0600

    header y footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,12 @@
 @endsection
 
 @section('content')
+	
+	<section style="min-height: 300px; border: 2px solid black; margin-top: 107px;">SECCION 1</section>
+	<section style="min-height: 300px; border: 2px solid black">SECCION 2</section>
+	<section style="min-height: 300px; border: 2px solid black">SECCION 3</section>
+	<section style="min-height: 300px; border: 2px solid black">SECCION 4</section>
+
 @endsection
 
 @section('modal-section')
--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,5 @@
-<footer class="footer mt-auto py-3 bg-light">
-	FOOTER
+<footer class="bg-dark text-center text-lg-start">
+	<div class="text-center p-3">
+		<span class="text-light">ICAM FILMS {{ date('Y') }}</span>
+	</div>
 </footer>
