c6ee060993ca2b9a143317a6705484e677f459f4
Enrique Rodriguez <enrique.rv0704@gmail.com>
2021-02-07 03:39:18 -0600

    contact

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -62,7 +62,7 @@ class FrontEndController extends Controller
         $mail->isSMTP();
 
         $mail->SMTPAuth = true;
-        $mail->SMTPDebug  = 3;
+        // $mail->SMTPDebug  = 4;
         $mail->SMTPSecure = env('MAIL_ENCRYPTION');
         $mail->Host = env('MAIL_HOST');
         $mail->Port = env('MAIL_PORT');
